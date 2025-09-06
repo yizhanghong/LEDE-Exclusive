55f171d5e278f9ddf3380a6e272469b919ecd713
firkerword <61251923+firkerword@users.noreply.github.com>
2025-09-06 15:01:19 +0800

    Update diy2305.sh

--- diy2305.sh
+++ diy2305.sh
@@ -38,6 +38,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-
 git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
 git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git  package/luci-theme-kucat
 git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/openwrt-nikki
+git clone https://github.com/timsaya/openwrt-bandix  package/openwrt-bandix 
 git clone https://github.com/timsaya/luci-app-bandix  package/luci-app-bandix 
 git clone https://github.com/sirpdboy/luci-app-netspeedtest  package/luci-app-netspeedtest
 # git clone https://github.com/Thaolga/luci-app-nekoclash  package/luci-app-nekoclash
